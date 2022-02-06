23ef416b5cbac1fb7b395a447ea44573ccbbaa77
Thinhbp <90796376+Thinhbp@users.noreply.github.com>
2022-02-06 12:53:28 +0700

    Update Swap.sol

--- Swap.sol
+++ Swap.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MintableToken is  ERC20 {
+contract Swap is  ERC20 {
 
     address owner ;
     constructor() public ERC20("vusd", "VUSD") { 
